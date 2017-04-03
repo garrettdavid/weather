f4660057d7e2f9c46f55d961eee3779bb492b4b0
Garrett David <garrett_david@hotmail.com>
2017-04-03 14:12:24 -0700

    fix button

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
-var locData;
-var city;
-var weatherUrl = "http://api.aerisapi.com/observations/";
-var weatherData;
-var tempF;
 var tempC;
-var tempIsF;
+var tempF;
+var weather;
+var tempIsF = true;
 
-//get location data and build URL for API
-$.getJSON("http://ip-api.com/json", function(data) {
-  locData = data;
-  city = data.city + ", " + data.region;
-  weatherUrl += city + "?client_id=2SvrQHFxcKgk9voYaw3VM&client_secret=N5YfLCpUo9m2vA6kJvhuUZysv36D8p6lH4x7MIij";
+$(function() {
+    $.getJSON("https://api.wunderground.com/api/40a2c598afbe8e7d/conditions/q/autoip.json", function(data) {
+        var location = data.current_observation.display_location.full;
+        weather = data.current_observation.weather;
+        tempF = data.current_observation.temp_f + "&deg;F";
+        tempC = data.current_observation.temp_c + "&deg;C";
+        $("#loc").text(location);
+        $("#temp").html('<h1 id="temp">Temperature: ' + weather + ' & ' + tempF + '</h1>');
+    });
 
-//get weather data from API
-  $.ajax({
-    url: weatherUrl,
-    dataType: "jsonp",
-    success: function(data) {
-      weatherData = data.response.ob;
-      tempF = weatherData.tempF + "&deg;F";
-      tempC = weatherData.tempC + "&deg;C";
-      $("#loc").text(city);
-      $("#temp").html('Temperature: ' + weatherData.weather + ' & ' + tempF);
-      $("#icon").attr("src", "Assets/Aeris_WxIcons_110x110/" + weatherData.icon);
-      tempIsF = true;
-    }
-  });
-
-  $("#tempBtn").click(changeTemp);
+    $("#tempBtn").click(function() {
+        if (tempIsF) {
+            $("#temp").html('<h1 id="temp">Temperature: ' + weather + ' & ' + tempC + '</h1>');
+            tempIsF = false;
+        } else {
+            $("#temp").html('<h1 id="temp">Temperature: ' + weather + ' & ' + tempF + '</h1>');
+            tempIsF = true;
+        }
+    });
 });
-
-function changeTemp() {
-  if (tempIsF) {
-    $("#temp").html('Temperature: ' + weatherData.weather + ' & ' + tempC);
-    tempIsF = false;
-  } else {
-    $("#temp").html('Temperature: ' + weatherData.weather + ' & ' + tempF);
-      tempIsF = true;
-  }
-}
